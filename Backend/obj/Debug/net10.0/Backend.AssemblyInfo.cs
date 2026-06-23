65e1b5df97c6dd17843616a948f2868c8acf78d8
Agbabinathedev <agbabina5@gmail.com>
2026-06-23 19:51:30 +0100

    Flying High

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6813113a68e2c73e2805f5eb16df12b9164a55b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ce05622b5666ff4aca889ccbf77e9f1b24b958e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
